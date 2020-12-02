6a0209d95030b04aafe8f1de87453f9ed1e9de2d
OliveLeb <lebel.olivier@wanadoo.fr>
2020-12-02 20:48:21 +0100

    header nav desktop

--- js/index.js
+++ js/index.js
@@ -1,20 +1,36 @@
-const $menu = document.querySelector('.menu');
-$menu.addEventListener('click',()=> {
-    $menu.nextElementSibling.classList.toggle('menu-active');
+const $menubtn = document.querySelector('.menu-btn');
+$menubtn.addEventListener('click',()=> {
+    $menubtn.nextElementSibling.classList.toggle('menu-active');
+});
+
+const desktopSize = window.matchMedia("(min-width: 970px)");
+window.addEventListener('resize', ()=> {
+    if(desktopSize.matches) $menubtn.nextElementSibling.classList.remove('menu-active');
 })
 
-const API_KEY = '77d06390c6ab416b8b18815a188f1274' ;
+// const API_KEY = '77d06390c6ab416b8b18815a188f1274' ;
 
 
 
-const url = `http://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+// const url = `http://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+// const proxyurl = "https://cors-anywhere.herokuapp.com/";
+// const url = 'http://newsapi.org/v2/top-headlines?' +
+//           'country=fr&' +
+//           'apiKey=77d06390c6ab416b8b18815a188f1274';
 
+// let headers = new Headers();
+const url = 'https://jsonplaceholder.typicode.com/posts'
+
+// var req = new Request(proxyurl + url);
 var req = new Request(url);
-fetch(req,{
-            mode:'no-cors',
-            headers: {'Content-Type': 'application/json'}
-    })
-    .then((response) => {
-        console.log(response.json());
-    })
+fetch(url/*,{
+            method:'GET',
+            //mode:'no-cors',
+            //headers: {'Content-Type': 'application/json'}
+    }*/)
+    .then((response) => response.json())
+    .then(json => console.log(json))
+    .catch(err => {
+        console.log('bonjour',err)
+    });
 
