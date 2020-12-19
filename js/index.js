1389584096b8522f8fc903c1e793ba17236552d1
OliveLeb <lebel.olivier@wanadoo.fr>
2020-12-19 16:02:27 +0100

    css home page

--- js/index.js
+++ js/index.js
@@ -8,6 +8,7 @@ window.addEventListener('resize', ()=> {
     if(desktopSize.matches) $menubtn.nextElementSibling.classList.remove('menu-active');
 })
 
+
 // const API_KEY = '77d06390c6ab416b8b18815a188f1274' ;
 
 
@@ -19,18 +20,18 @@ window.addEventListener('resize', ()=> {
 //           'apiKey=77d06390c6ab416b8b18815a188f1274';
 
 // let headers = new Headers();
-const url = 'https://jsonplaceholder.typicode.com/posts'
+// const url = 'https://jsonplaceholder.typicode.com/posts'
 
-// var req = new Request(proxyurl + url);
-var req = new Request(url);
-fetch(url/*,{
-            method:'GET',
-            //mode:'no-cors',
-            //headers: {'Content-Type': 'application/json'}
-    }*/)
-    .then((response) => response.json())
-    .then(json => console.log(json))
-    .catch(err => {
-        console.log('bonjour',err)
-    });
+// // var req = new Request(proxyurl + url);
+// var req = new Request(url);
+// fetch(url/*,{
+//             method:'GET',
+//             //mode:'no-cors',
+//             //headers: {'Content-Type': 'application/json'}
+//     }*/)
+//     .then((response) => response.json())
+//     .then(json => console.log(json))
+//     .catch(err => {
+//         console.log('bonjour',err)
+//     });
 
